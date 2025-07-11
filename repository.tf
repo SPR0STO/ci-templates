8dd61bfa4f5507d619078d38b607b3755b4bcb7a
Sergey PR0STO <pr0sto@hotmail.com>
2025-07-11 19:10:44 +0300

    Refactor workflow into atomic linter jobs

--- repository.tf
+++ repository.tf
@@ -29,12 +29,11 @@ resource "github_branch_protection" "protected_branches" {
   required_pull_request_reviews {
     dismiss_stale_reviews           = true
     require_code_owner_reviews      = true
-    required_approving_review_count = 1
+    required_approving_review_count = 0
   }
 
   required_status_checks {
     strict   = true
-    contexts = ["ci/linters"]
+    contexts = []
   }
-
 }
